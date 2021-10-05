d7d12b89cdafdb09abdb8b6d5ea2c025d425ffaf
Chris Dueck <chris.dueck@wattpad.com>
2021-10-05 11:46:14 -0400

    Update docker image tagging
    Docker tags must be well-ordered for flux's image update automation to
    work.

--- publish_image.sh
+++ publish_image.sh
@@ -9,9 +9,12 @@ if [ "$TRAVIS_BRANCH $TRAVIS_PULL_REQUEST" == "master false" ]; then
 
   export GIT_REVISION=$(git rev-parse HEAD)
   export REPO="723255503624.dkr.ecr.us-east-1.amazonaws.com/kamu"
+
+  timestamp=$(date +%s)
+
   docker tag kamu:base kamu:$GIT_REVISION
-  docker tag kamu:$GIT_REVISION $REPO:$GIT_REVISION
+  docker tag kamu:$GIT_REVISION $REPO:$GIT_REVISION-${timestamp}
   docker tag kamu:$GIT_REVISION $REPO:latest
-  docker push $REPO:$GIT_REVISION
+  docker push $REPO:$GIT_REVISION-${timestamp}
   docker push $REPO:latest
 fi
