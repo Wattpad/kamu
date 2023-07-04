83d31a3a4c7cd882221d0bcc1d2b57bb92f4d869
Rohan Poojary <rohan@wattpad.com>
2023-07-04 15:28:31 -0400

    [PI-2348] Fix spacing

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ connStatus.start();
 var server = Http.createServer( function( req, res ) {
   // only get requests with a url pathname
   if ( req.method !== 'GET' || req.url === '/' ) {
-    res.writeHead( 301, {...config.defaultHeaders, 'Location': 'https://www.wattpad.com'});
+    res.writeHead( 301, { ...config.defaultHeaders, 'Location': 'https://www.wattpad.com' });
     return res.end();
   }
   // favicon requests will get a straight response
